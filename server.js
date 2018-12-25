6fbec2f2681501278207db4ca06b863379f3a375
mmannava <manasapriyamvadamannava@yahoo.com>
2018-12-25 17:37:32 -0600

    added other fields

--- server.js
+++ server.js
@@ -9,15 +9,27 @@ app.use(express.json());
 
 const foods = [{
         id: 1,
-        name: 'Acai Bowl'
+        name: 'Acai Bowl',
+        description: 'a rich blend of cocoa with acai seeds',
+        carbs: 21,
+        proteins: 20,
+        fats: 9
     },
     {
         id: 2,
-        name: 'Quest Protein Bar'
+        name: 'Quest Protein Bar',
+        description: 'a healthy protein snack',
+        carbs: 9,
+        proteins: 12,
+        fats: 2
     },
     {
         id: 3,
-        name: 'Quinoa & Brown Rice'
+        name: 'Quinoa & Brown Rice',
+        description: 'rich in proteins vitamins & minerals',
+        carbs: 33,
+        proteins: 15,
+        fats: 3
     }
 ];
 
@@ -36,7 +48,11 @@ app.get('/foods/:id', (req, res) => {
 app.post('/foods', (req, res) => {
     const food = {
         id: foods.length + 1,
-        name: req.body.name
+        name: req.body.name,
+        description: req.body.description,
+        carbs: req.body.carbs,
+        proteins: req.body.proteins,
+        fats: req.body.fats
     }
     foods.push(food);
     res.send(food);
@@ -48,6 +64,10 @@ app.put('/foods/:id', (req, res) => {
     if (!food) return res.status(404).send('The food with the given ID was not found.');
   
     food.name = req.body.name;
+    food.description = req.body.description;
+    food.carbs = req.body.carbs;
+    food.proteins = req.body.proteins;
+    food.fats = req.body.fats;
     res.send(food);
 });
 
