58d7d24db569c8b84e8d88b720df37ced7fd139c
mmannava <manasapriyamvadamannava@yahoo.com>
2018-12-24 21:05:38 -0600

    added foods end point

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Welcome to food rest api');
 });
- 
+  
+app.get('/foods', (req, res) => {
+    res.send(['Acai Bowl', 'Quest Protein Bar', 'Quinoa & Brown Rice']);
+});
+
 app.listen(5689, () => console.log('Listening on port 5689'));
