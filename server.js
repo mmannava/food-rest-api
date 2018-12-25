d853b8cae8a1f55fcfcc27d2111dc83293d407fe
mmannava <manasapriyamvadamannava@yahoo.com>
2018-12-24 21:29:47 -0600

    added get food by id

--- server.js
+++ server.js
@@ -5,8 +5,30 @@ app.get('/', (req, res) => {
     res.send('Welcome to food rest api');
 });
   
+const foods = [{
+        id: 1,
+        name: 'Acai Bowl'
+    },
+    {
+        id: 2,
+        name: 'Quest Protein Bar'
+    },
+    {
+        id: 3,
+        name: 'Quinoa & Brown Rice'
+    }
+];
+
 app.get('/foods', (req, res) => {
-    res.send(['Acai Bowl', 'Quest Protein Bar', 'Quinoa & Brown Rice']);
+    res.send(foods);
 });
 
-app.listen(5689, () => console.log('Listening on port 5689'));
+// get food by id
+app.get('/foods/:id', (req, res) => {
+    const food = foods.find(g => g.id === parseInt(req.params.id));
+    if (!food) return res.status(404).send('The food with the given ID was not found.');
+    res.send(food);
+});
+
+const port = process.env.PORT || 5689;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
